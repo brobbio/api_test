b80b1c0914a6725ac9d84659ed294e580bd2ff68
brobbio <69459973+brobbio@users.noreply.github.com>
2026-06-02 15:52:12 -0300

    New release with dockerized app

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="items-api",
-    version="0.1.0",
+    version="0.2.0",
     description="Webapp to store items",
     package_dir={"": "."},
     author="",
